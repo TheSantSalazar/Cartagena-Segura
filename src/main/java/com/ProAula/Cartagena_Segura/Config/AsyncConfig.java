b89496f89e013a36243b048461cd6581ac6dd0af
Santiago Salazar Orozco <salazarorozco2121@gmail.com>
2026-03-22 14:38:54 -0500

    Add AsyncConfig class for asynchronous processing

---
+++ src/main/java/com/ProAula/Cartagena_Segura/Config/AsyncConfig.java
@@ -0,0 +1,8 @@
+package com.ProAula.Cartagena_Segura.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {}
